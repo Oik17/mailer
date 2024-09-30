9596ce218b55b01c914d3dee9e1cd8698e3d5293
Akshat Gupta <akshat.g1707@gmail.com>
2024-09-30 22:59:43 +0530

    feat: mailer

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net/smtp"
+	"os"
+	"text/template"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
+
+	from := "oikshut@gmail.com"
+	password := os.Getenv("password")
+
+	to := []string{
+		"oikshut1@gmail.com",
+	}
+
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "587"
+
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	t, _ := template.ParseFiles("template.html")
+
+	var body bytes.Buffer
+
+	mimeHeaders := "MIME-version: 1.0;\nContent-Type: text/html; charset=\"UTF-8\";\n\n"
+	body.Write([]byte(fmt.Sprintf("Subject: This is a test subject \n%s\n\n", mimeHeaders)))
+
+	t.Execute(&body, struct {
+		Name    string
+		Message string
+	}{
+		Name:    "Puneet Singh",
+		Message: "This is a test message in a HTML template",
+	})
+
+	err = smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, body.Bytes())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Email Sent!")
+}
